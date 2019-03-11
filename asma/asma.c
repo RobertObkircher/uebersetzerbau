010956ac5365ac1a87337375501a86db79e4eb7a
Robert Obkircher <robert.obkircher@gmail.com>
2019-03-11 17:41:08 +0100

    comment asma

--- asma/asma.c
+++ asma/asma.c
@@ -1,7 +1,10 @@
+// this was compiled with `gcc -O -S`
 void asma(unsigned long *a, unsigned long *b, unsigned long c) {
     unsigned long longs = c >> 6;
     unsigned long bits = c & 63;
 
+    // removed the if
+    
     b[0] = (a[(0-longs)%2]<<bits)|(a[(-1-longs)%2]>>-bits);
     b[1] = (a[(1-longs)%2]<<bits)|(a[( 0-longs)%2]>>-bits);
 }
