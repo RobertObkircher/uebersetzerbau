35c946173af11fe905120c08f92f153edd15f94d
Robert Obkircher <robert.obkircher@gmail.com>
2019-03-12 18:02:01 +0100

    started implementing asmb

--- asmb/asmb.s
+++ asmb/asmb.s
@@ -5,73 +5,35 @@
 asmb: # rdi *a, rsi *b, rdx c, rcx n
 .LFB0:
 	.cfi_startproc
-	movq	%rcx, %r9
-	movq	%rdx, %rax
-	shrq	$6, %rax
-	andl	$63, %edx
-	je	.L2
-	movq	%rdx, %r10
-	testq	%rcx, %rcx
-	je	.L12
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	pushq	%rbx
-	.cfi_def_cfa_offset 24
-	.cfi_offset 3, -24
-	movq	%rax, %r8
-	negq	%r8
-	movq	%rcx, %rbp
-	subq	%rax, %rbp
-	leaq	(%rsi,%rax,8), %rbx
-	movl	%edx, %r11d
-	negl	%r11d
-.L6:
-	leaq	-1(%r8), %rax
-	movl	$0, %edx
-	divq	%r9
-	movq	(%rdi,%rdx,8), %rsi
-	movl	%r11d, %ecx
-	shrq	%cl, %rsi
-	movq	%r8, %rax
-	movl	$0, %edx
-	divq	%r9
-	movq	(%rdi,%rdx,8), %rax
-	movl	%r10d, %ecx
-	salq	%cl, %rax
-	orq	%rax, %rsi
-	movq	%rsi, (%rbx,%r8,8)
-	addq	$1, %r8
-	cmpq	%rbp, %r8
-	jne	.L6
-	popq	%rbx
-	.cfi_def_cfa_offset 16
-	popq	%rbp
-	.cfi_def_cfa_offset 8
-	ret
-.L12:
-	.cfi_restore 3
-	.cfi_restore 6
-	ret
-.L2:
-	testq	%rcx, %rcx
-	je	.L10
-	movq	%rax, %rcx
-	negq	%rcx
-	movq	%r9, %r8
-	subq	%rax, %r8
-	leaq	(%rsi,%rax,8), %rsi
-.L5:
-	movq	%rcx, %rax
-	movl	$0, %edx
-	divq	%r9
-	movq	(%rdi,%rdx,8), %rax
-	movq	%rax, (%rsi,%rcx,8)
-	addq	$1, %rcx
-	cmpq	%rcx, %r8
-	jne	.L5
-	ret
-.L10:
+
+        # 1111 2222 3333 (if long was 4 bits)
+        # 1112 2223 3331 (0 longs, 1 bits)
+        # 2223 3331 1112 (1 longs, 1 bits)
+        # 3331 1112 2223 (2 longs, 1 bits)
+
+        # rax    n-1 (n is a power of two, so this simplifies modulo n)
+        # rcx    n; also serves as loop counter
+        # rdx    c
+        # rsi   *b
+        # rdi   *a
+        # r8     relative_read_position
+        # r9     relative_write_position
+        # r10    first_read_value
+        # r11    to_write
+
+        # r12    number_of_longs
+
+        pushq %r12
+
+        movq %rcx, %rax
+        dec %rax
+        jz .EXIT
+.LOOP:
+        #TODO 
+        loop .LOOP
+.EXIT:
+
+        popq %r12
 	ret
 	.cfi_endproc
 .LFE0:
