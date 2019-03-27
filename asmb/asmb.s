914899e1ba137b00cec059b2d7317a597e76395f
Robert Obkircher <robert.obkircher@gmail.com>
2019-03-27 21:00:46 +0100

    asmb

--- asmb/asmb.s
+++ asmb/asmb.s
@@ -11,29 +11,58 @@ asmb: # rdi *a, rsi *b, rdx c, rcx n
         # 2223 3331 1112 (1 longs, 1 bits)
         # 3331 1112 2223 (2 longs, 1 bits)
 
-        # rax    n-1 (n is a power of two, so this simplifies modulo n)
-        # rcx    n; also serves as loop counter
-        # rdx    c
+        # result[n] = source[n+longs %] <<<< source[n+longs+1 %]
+
+        # rax    loop counter n-1..0
+        # rcx    number of bits to shift within a long
+        # rdx    number_of_longs
         # rsi   *b
         # rdi   *a
         # r8     relative_read_position
-        # r9     relative_write_position
-        # r10    first_read_value
-        # r11    to_write
+        # r9     n-1 (n is a power of two, so this simplifies modulo n)
+        # r10    shifted_into_r11
+        # r11    result
 
-        # r12    number_of_longs
+        #pushq %r12
 
-        pushq %r12
-
+        # compute n-1 in %rax
         movq %rcx, %rax
+        jz exit # n = 0
         dec %rax
-        jz .EXIT
-.LOOP:
-        #TODO 
-        loop .LOOP
-.EXIT:
 
-        popq %r12
+        # store n-1 to compute modulo n
+        movq %rax, %r9
+
+        # bits
+        movq %rdx, %rcx
+        and $63, %rcx
+
+        # compute number_of_longs
+        shr $6, %rdx
+        
+        # compute initial relative_read_position
+        # (n+longs+1)&(n-1)
+        movq %rdx, %r8
+        inc %r8
+        and %r9, %r8
+
+loop:
+        # read first value from relative_read_position
+        movq (%rdi, %r8), %r10
+        dec %r8
+        and %rax, %r8
+
+        # read the second value from relative_read_position
+        movq (%rdi, %r8), %r11
+
+        shld %cl, %r10, %r11
+        movq %r11, (%rsi, %rax)
+
+        add $-1, %rax
+        jnz loop
+exit:
+
+        #popq %r12
 	ret
 	.cfi_endproc
 .LFE0:
