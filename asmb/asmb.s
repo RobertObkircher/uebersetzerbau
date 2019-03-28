165f1a678d1bada055589b3e9172d94758f37966
Robert Obkircher <robert.obkircher@gmail.com>
2019-03-28 08:27:23 +0100

    improved comments

--- asmb/asmb.s
+++ asmb/asmb.s
@@ -5,20 +5,12 @@
 asmb: # rdi *a, rsi *b, rdx c, rcx n
 .LFB0:
 	.cfi_startproc
-
-        # 1111 2222 3333 (if long was 4 bits)
-        # 1112 2223 3331 (0 longs, 1 bits)
-        # 2223 3331 1112 (1 longs, 1 bits)
-        # 3331 1112 2223 (2 longs, 1 bits)
-
-        # result[n] = source[n+longs %] <<<< source[n+longs+1 %]
-
         # rax    loop counter n-1..0
         # rcx    number of bits to shift within a long
         # rdx    number_of_longs
-        # rsi   *b
-        # rdi   *a
-        # r8     relative_read_position
+        # rsi    *b
+        # rdi    *a
+        # r8     relative_read_position ((loop_counter-longs)%n)
         # r9     n-1 (n is a power of two, so this simplifies modulo n)
         # r10    shifted_into_r11
         # r11    result
@@ -26,32 +18,33 @@ asmb: # rdi *a, rsi *b, rdx c, rcx n
         # compute n-1 in %rax
         movq %rcx, %rax
         add $-1, %rax
-        js exit
+        js exit # n = 0
 
-        # store n-1 to compute modulo n
+        # store n-1, so we can compute %n with &(n-1)
         movq %rax, %r9
 
         # bits
         movq %rdx, %rcx
         and $63, %rcx
 
-        # compute number_of_longs
+        # number_of_longs
         shr $6, %rdx
         
-        # compute initial relative_read_position
+        # relative_read_position
         movq %rax, %r8
         sub %rdx, %r8
         and %r9, %r8
 
 loop:
-        # read first value from relative_read_position
+        # a[(i-longs)%n]
         movq (%rdi, %r8, 8), %r11
+
+        # a[(i-longs-1)%n]
         dec %r8
         and %r9, %r8 # %n
-
-        # read the second value from relative_read_position
         movq (%rdi, %r8, 8), %r10
 
+        # b[i] = a[(i-longs)%n] `shld` a[(i-longs-1)%n]
         shld %cl, %r10, %r11
         movq %r11, (%rsi, %rax, 8)
 
